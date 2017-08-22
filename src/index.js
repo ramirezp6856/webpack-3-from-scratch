6bf74da70d2aaa999c625708d42205c708d0487e
Phil Ramirez <ramirezp6856@gmail.com>
2017-08-22 09:29:27 -0400

    Added source maps and watch mode

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ function component() {
     var btn = document.createElement('button');
 
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = _.join(['Hello!', 'webpack'], ' ');
 
     btn.innerHTML = 'Click me and check the console!';
     btn.onclick = printMe;
