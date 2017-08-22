23b5b5133ee373337168ff55057e56d78b8e3c7d
Phil Ramirez <ramirezp6856@gmail.com>
2017-08-22 02:04:13 -0400

    Loading images and Loading Data via web pack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 const path = require('path');
 
+const PATHS = {
+    build: path.join(__dirname, './dist/')
+}
+
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: PATHS.build
     },
     module: {
         rules: [
@@ -13,6 +18,24 @@ module.exports = {
                 use: [
                     'style-loader',
                     'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(csv|tsv)$/,
+                use: [
+                    'csv-loader'
+                ]
+            },
+            {
+                test: /\.xml$/,
+                use: [
+                    'xml-loader'
                 ]
             }
         ]
