b5a1dab95b2730781955a0e1201cde666f2a8a47
Phil Ramirez <ramirezp6856@gmail.com>
2017-08-22 02:27:01 -0400

    Preparing for Output Management

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,9 @@
 const path = require('path');
 
-const PATHS = {
-    build: path.join(__dirname, './dist/')
-}
-
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: PATHS.build
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    'file-loader'
-                ]
-            },
-            {
-                test: /\.(csv|tsv)$/,
-                use: [
-                    'csv-loader'
-                ]
-            },
-            {
-                test: /\.xml$/,
-                use: [
-                    'xml-loader'
-                ]
-            }
-        ]
+        path: path.resolve(__dirname, 'dist')
     }
 };
